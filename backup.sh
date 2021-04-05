d688e754d752e7a99e1880ce9b1fa50bdd31ec04
pbirkle <internet@birkle.email>
2021-04-05 18:32:31 +0200

    various improvements

--- backup.sh
+++ backup.sh
@@ -1,37 +1,60 @@
 #!/bin/bash
 
-function print_banner {
+function print_banner() {
   echo ""
-  echo "████████╗ █████╗ ██████╗"
-  echo "╚══██╔══╝██╔══██╗██╔══██╗"
-  echo "   ██║   ███████║██████╔╝"
-  echo "   ██║   ██╔══██║██╔══██╗"
-  echo "   ██║   ██║  ██║██║  ██║"
-  echo "   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝"
-  echo "██████╗  █████╗  ██████╗    ██╗██████╗ ███████╗███████╗"
-  echo "██╔══██╗██╔══██╗██╔════╝   ██╔╝██╔══██╗██╔════╝██╔════╝"
-  echo "██████╔╝███████║██║       ██╔╝ ██████╔╝█████╗  ███████╗"
-  echo "██╔══██╗██╔══██║██║      ██╔╝  ██╔══██╗██╔══╝  ╚════██║"
-  echo "██████╔╝██║  ██║╚██████╗██╔╝   ██║  ██║███████╗███████║"
-  echo "╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝    ╚═╝  ╚═╝╚══════╝╚══════╝"
+  echo " ████████╗ █████╗ ██████╗     ██████╗ ███████╗"
+  echo " ╚══██╔══╝██╔══██╗██╔══██╗   ██╔════╝ ╚══███╔╝"
+  echo "    ██║   ███████║██████╔╝   ██║  ███╗  ███╔╝"
+  echo "    ██║   ██╔══██║██╔══██╗   ██║   ██║ ███╔╝"
+  echo "    ██║   ██║  ██║██║  ██║██╗╚██████╔╝███████╗"
+  echo "    ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝ ╚══════╝"
+  echo " ██████╗  █████╗  ██████╗██╗  ██╗██╗   ██╗██████╗"
+  echo " ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██║   ██║██╔══██╗"
+  echo " ██████╔╝███████║██║     █████╔╝ ██║   ██║██████╔╝"
+  echo " ██╔══██╗██╔══██║██║     ██╔═██╗ ██║   ██║██╔═══╝"
+  echo " ██████╔╝██║  ██║╚██████╗██║  ██╗╚██████╔╝██║"
+  echo " ╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝"
+  echo " https://github.com/pbirkle/tar-backup-restore"
   echo ""
 }
 
-function print_usage {
+function print_usage() {
   print_banner
-  echo "Usage:"
-  echo "-d [value] | mandatory | path to destination directory (to store backup.tar.gz)"
-  echo "-s [value] | mandatory | path to source directory or source file, can be used multiple times"
-  echo "-v         | optional  | skip verification (assume everything is set correctly)"
+
+  echo " - backups are created as a compressed tar file (tar.gz, save space on your hard drive)"
+  echo " - backups are created on a monthly base"
+  echo "   - every first backup (per month) will be an full backup"
+  echo "   - every other backup (per month) will be an incremental backup"
+  echo " - to restore a backup use 'tar.gz restore'"
+  echo ""
+
+  bold='\033[1m'
+  reset="\e[0m"
+  echo " |----------------------------------------------------------------------------------------------|"
+  echo -e " | ${bold}Parameter${reset}  | ${bold}Mandatory${reset} | ${bold}Comment${reset}                                                             |"
+  echo " |----------------------------------------------------------------------------------------------|"
+  echo " | -d [value] | yes       | path to destination directory (to store *.tar.gz)                   |"
+  echo " |----------------------------------------------------------------------------------------------|"
+  echo " | -s [value] | yes       | path to source directory or source file, can be used multiple times |"
+  echo " |----------------------------------------------------------------------------------------------|"
+  echo " | -v         | no        | skip verification (assume everything is set correctly)              |"
+  echo " |----------------------------------------------------------------------------------------------|"
 }
 
-function print_error {
+# $1: error statement
+function print_error() {
   red="\e[0;91m"
   reset="\e[0m"
   echo -e "${red}${1}${reset}"
 }
 
-function setup_environment {
+# $1: log statement
+function print_log() {
+  echo "${1}"
+  echo "${1}" >>"${LOG_FILEPATH}"
+}
+
+function setup_environment() {
   if [[ $(id -u) -ne 0 ]]; then
     print_error "script was not executed with root privileges (use sudo)"
     exit 1
@@ -40,13 +63,13 @@ function setup_environment {
   if [[ -z "${DEST_DIR}" ]]; then
     print_error "destination directory not set (see -d flag)"
     print_usage
-    exit 1;
+    exit 1
   fi
 
   if [[ ${#SOURCES[@]} -lt 1 ]]; then
     print_error "no source directories or source files set (see -s flag)"
     print_usage
-    exit 1;
+    exit 1
   fi
 
   # check if first character is slash
@@ -60,7 +83,7 @@ function setup_environment {
       exit 1
     elif [[ ! -e "${SOURCE}" ]]; then
       print_error "source '${SOURCE}' does not exist"
-      exit 1;
+      exit 1
     fi
   done
 
@@ -70,19 +93,18 @@ function setup_environment {
     DEST_DIR=$(echo "${DEST_DIR}" | sed 's/.\{1\}$//')
   fi
 
-  readonly YEAR=$(date +"%Y")
-  readonly WEEK="CW$(date +"%V")"
+  readonly YEAR="$(date +"%Y")"
+  readonly MONTH="$(date +"%m")"
   readonly DATE_TIME="$(date +"%Y-%m-%d_%H-%M-%S")"
-
-  readonly BACKUP_DIRECTORY="${DEST_DIR}/${YEAR}-${WEEK}"
+  readonly BACKUP_DIRECTORY="${DEST_DIR}/${YEAR}-${MONTH}"
   readonly BACKUP_FILENAME="${DATE_TIME}.tar.gz"
-  readonly SNAPSHOT_FILENAME="${YEAR}-${WEEK}.snapshot"
   readonly BACKUP_FILEPATH="${BACKUP_DIRECTORY}/${BACKUP_FILENAME}"
-  readonly BACKUP_LOG_FILEPATH="${BACKUP_FILEPATH}.log"
+  readonly SNAPSHOT_FILENAME="${YEAR}-${MONTH}.snapshot"
   readonly SNAPSHOT_FILEPATH="${BACKUP_DIRECTORY}/${SNAPSHOT_FILENAME}"
+  readonly LOG_FILEPATH="${BACKUP_FILEPATH}.log"
 
   if [[ ! -e "${BACKUP_DIRECTORY}" ]]; then
-    mkdir -p "${BACKUP_DIRECTORY}" || (echo "could not create destination backup directory" && exit 1)
+    mkdir -p "${BACKUP_DIRECTORY}" || (print_error "could not create destination backup directory" && exit 1)
   fi
 
   if [[ -e "${BACKUP_FILEPATH}" ]]; then
@@ -91,12 +113,19 @@ function setup_environment {
   fi
 }
 
-function create_backup {
-  echo "Using following specification"
-  echo "destination directory:  ${DEST_DIR}"
-  echo "backup filename:        ${BACKUP_FILENAME}"
-  echo "sources:                ${SOURCES[*]}"
-  echo ""
+function verify() {
+  print_log ""
+  print_log "backup file:"
+  print_log "  - ${BACKUP_FILEPATH}"
+  print_log ""
+  print_log "backup log (full):"
+  print_log "  - ${LOG_FILEPATH}"
+  print_log ""
+  print_log "sources:"
+  for SOURCE in "${SOURCES[@]}"; do
+    print_log "  - ${SOURCE}"
+  done
+  print_log ""
 
   if [[ -z "${VERIFY}" ]]; then
     read -r -p "do you really want to execute a backup with above specification: [n|y] " VERIFY
@@ -107,21 +136,24 @@ function create_backup {
     print_error "backup verification failed"
     exit 1
   fi
+}
 
-  echo "backup started, this could take a while..."
-  tar --listed-incremental="${SNAPSHOT_FILEPATH}" -cvzf "${BACKUP_FILEPATH}" ${SOURCES[*]} > "${BACKUP_LOG_FILEPATH}" || (print_error "backup failed" && exit 1)
-  echo "backup successfully, see '${BACKUP_LOG_FILEPATH}' for further investigation"
+function create_backup() {
+  print_log "backup process started, this could take a while..."
+  tar --listed-incremental="${SNAPSHOT_FILEPATH}" -cvzf "${BACKUP_FILEPATH}" ${SOURCES[*]} &>>"${LOG_FILEPATH}" || (print_error "backup failed" && exit 1)
+  print_log "backup process successful!"
 }
 
 while getopts d:s:v option; do
   case "${option}" in
 
   d) DEST_DIR=${OPTARG} ;;
-  s) SOURCES=( "${SOURCES[@]}" "${OPTARG}" ) ;;
+  s) SOURCES=("${SOURCES[@]}" "${OPTARG}") ;;
   v) VERIFY=y ;;
   *) print_usage && exit 1 ;;
   esac
 done
 
 setup_environment
+verify
 create_backup
--- restore.sh
+++ restore.sh
@@ -1,37 +1,60 @@
 #!/bin/bash
 
-function print_banner {
+function print_banner() {
   echo ""
-  echo "████████╗ █████╗ ██████╗"
-  echo "╚══██╔══╝██╔══██╗██╔══██╗"
-  echo "   ██║   ███████║██████╔╝"
-  echo "   ██║   ██╔══██║██╔══██╗"
-  echo "   ██║   ██║  ██║██║  ██║"
-  echo "   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝"
-  echo "██████╗  █████╗  ██████╗    ██╗██████╗ ███████╗███████╗"
-  echo "██╔══██╗██╔══██╗██╔════╝   ██╔╝██╔══██╗██╔════╝██╔════╝"
-  echo "██████╔╝███████║██║       ██╔╝ ██████╔╝█████╗  ███████╗"
-  echo "██╔══██╗██╔══██║██║      ██╔╝  ██╔══██╗██╔══╝  ╚════██║"
-  echo "██████╔╝██║  ██║╚██████╗██╔╝   ██║  ██║███████╗███████║"
-  echo "╚═════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝    ╚═╝  ╚═╝╚══════╝╚══════╝"
+  echo " ████████╗ █████╗ ██████╗     ██████╗ ███████╗"
+  echo " ╚══██╔══╝██╔══██╗██╔══██╗   ██╔════╝ ╚══███╔╝"
+  echo "    ██║   ███████║██████╔╝   ██║  ███╗  ███╔╝"
+  echo "    ██║   ██╔══██║██╔══██╗   ██║   ██║ ███╔╝"
+  echo "    ██║   ██║  ██║██║  ██║██╗╚██████╔╝███████╗"
+  echo "    ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝ ╚══════╝"
+  echo " ██████╗ ███████╗███████╗████████╗ ██████╗ ██████╗ ███████╗"
+  echo " ██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗██╔════╝"
+  echo " ██████╔╝█████╗  ███████╗   ██║   ██║   ██║██████╔╝█████╗"
+  echo " ██╔══██╗██╔══╝  ╚════██║   ██║   ██║   ██║██╔══██╗██╔══╝"
+  echo " ██║  ██║███████╗███████║   ██║   ╚██████╔╝██║  ██║███████╗"
+  echo " ╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝"
+  echo " https://github.com/pbirkle/tar-backup-restore"
   echo ""
 }
 
-function print_usage {
+function print_usage() {
   print_banner
-  echo "Usage:"
-  echo "-b [value] | mandatory | path to backup directory (containing the *.tar.gz files)"
-  echo "-d [value] | optional  | path to destination directory (if not present files will be extracted corresponding to root directory)"
-  echo "-v         | optional  | skip verification (assume everything is set correctly)"
+
+  echo " - backups have to be created with 'tar.gz backup'"
+  echo ""
+
+  bold='\033[1m'
+  reset="\e[0m"
+  echo " |----------------------------------------------------------------------------------------------|"
+  echo -e " | ${bold}Parameter${reset}  | ${bold}Mandatory${reset} | ${bold}Comment${reset}                                                             |"
+  echo " |----------------------------------------------------------------------------------------------|"
+  echo " | -b [value] | yes       | path to backup directory (containing the *.tar.gz files)            |"
+  echo " |----------------------------------------------------------------------------------------------|"
+  echo " | -d [value] | no        | path to destination directory                                       |"
+  echo " |            |           | default: root directory (/)                                         |"
+  echo " |----------------------------------------------------------------------------------------------|"
+  echo " | -l [value] | no        | filename (!) of last backup file to restore                         |"
+  echo " |            |           | default: all files                                                  |"
+  echo " |----------------------------------------------------------------------------------------------|"
+  echo " | -v         | no        | skip verification (assume everything is set correctly)              |"
+  echo " |----------------------------------------------------------------------------------------------|"
 }
 
-function print_error {
+# $1: error statement
+function print_error() {
   red="\e[0;91m"
   reset="\e[0m"
   echo -e "${red}${1}${reset}"
 }
 
-function setup_environment {
+# $1: log statement
+function print_log() {
+  echo "${1}"
+  echo "${1}" >>"${LOG_FILEPATH}"
+}
+
+function setup_environment() {
   if [[ $(id -u) -ne 0 ]]; then
     print_error "script was not executed with root privileges (use sudo)"
     exit 1
@@ -77,13 +100,34 @@ function setup_environment {
     exit 1
   fi
 
+  if ! [[ "${BACKUP_FILES[*]}" =~ ${LAST_BACKUP_FILE} ]]; then
+    print_error "last backup file '${LAST_BACKUP_FILE}' could not be found"
+    exit 1
+  fi
+
+  readonly DATE_TIME="$(date +"%Y-%m-%d_%H-%M-%S")"
+  readonly LOG_FILEPATH="/tmp/tar-restore-${DATE_TIME}.log"
 }
 
-function restore_backup {
-  echo "Using following specification"
-  echo "backup directory:  ${BACKUP_DIR}"
-  echo "backup files:      ${BACKUP_FILES[*]}"
-  echo ""
+function verify() {
+  print_log ""
+  print_log "restore base directory:"
+  print_log "  - ${DEST_DIR}"
+  print_log ""
+  print_log "restore log (full):"
+  print_log "  - ${LOG_FILEPATH}"
+  print_log ""
+  print_log "backup directory:"
+  print_log "  - ${BACKUP_DIR}"
+  print_log ""
+  print_log "backup files:"
+  for BACKUP_FILE in "${BACKUP_FILES[@]}"; do
+    print_log "  - ${BACKUP_FILE}"
+    if [[ "${BACKUP_FILE}" == "${LAST_BACKUP_FILE}" ]]; then
+      break;
+    fi
+  done
+  print_log ""
 
   if [[ -z "${VERIFY}" ]]; then
     read -r -p "do you really want to execute a backup with above specification: [n|y] " VERIFY
@@ -94,24 +138,35 @@ function restore_backup {
     print_error "backup verification failed"
     exit 1
   fi
-
-  for BACKUP_FILE in "${BACKUP_FILES[@]}"; do
-    BACKUP_FILEPATH="${BACKUP_DIR}/${BACKUP_FILE}"
-    echo "restoring '${BACKUP_FILEPATH}' to '${DEST_DIR}' ..."
-    tar --listed-incremental=/dev/null -xvzf "${BACKUP_FILEPATH}" -C "${DEST_DIR}"
-    echo ""
-  done
 }
 
-while getopts b:d:s:v option; do
+function restore_backup() {
+  print_log "restore process started, this could take a while..."
+  COUNT=1
+  for BACKUP_FILE in "${BACKUP_FILES[@]}"; do
+    BACKUP_FILEPATH="${BACKUP_DIR}/${BACKUP_FILE}"
+    print_log "restoring '${BACKUP_FILEPATH}' to '${DEST_DIR}' ..."
+    tar --listed-incremental=/dev/null -xvzf "${BACKUP_FILEPATH}" -C "${DEST_DIR}" &>>"${LOG_FILEPATH}"
+    if [[ "${BACKUP_FILE}" == "${LAST_BACKUP_FILE}" ]] && [[ ${COUNT} -lt ${#BACKUP_FILES[@]} ]]; then
+      print_log "reached last backup file ${LAST_BACKUP_FILE}, skip remaining backup files"
+      break;
+    fi
+    ((COUNT++))
+  done
+  print_log "restore successful"
+}
+
+while getopts b:d:l:s:v option; do
   case "${option}" in
 
   b) BACKUP_DIR=${OPTARG} ;;
   d) DEST_DIR=${OPTARG} ;;
+  l) LAST_BACKUP_FILE=${OPTARG} ;;
   v) VERIFY=y ;;
   *) print_usage && exit 1 ;;
   esac
 done
 
 setup_environment
+verify
 restore_backup
